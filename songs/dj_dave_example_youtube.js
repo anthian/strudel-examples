e52775e198347fbe5160adb67074e42033867881
Anthian85 <raghnarok2002@gmail.com>
2025-08-06 16:53:14 +0200

    Minor variations on visuals and fixed import error

--- songs/dj_dave_example_youtube.js
+++ songs/dj_dave_example_youtube.js
@@ -1,7 +1,8 @@
- setCps(140/60/4)
+setCps(140/60/4)
 
-//samples('github:algorave-dave/samples')
-//samples('github:tidalcycles/dirt-samples')
+samples('github:tidalcycles/dirt-samples')
+
+const gooo = 2
 
 const gainnn = [
   "2",
@@ -15,17 +16,15 @@ const Structures = [
   "{x ~!9 x ~!15 x ~ x ~!7 x ~!3 < ~ x > ~}%16"
 ]
 
-const gooo = 2
-
 bassline: note("[eb1, eb2]!16 [f2, f1]!16 [g2, g1]!16 [f2, f1]!8 [bb2, bb1]!8")
   .sound("supersaw")
   .slow(8)
   .postgain(2)
+  .lpf(slider(1561.4,300,2000))
   .room(0.6)
-  .lpf(slider(789.6,300,2000))
-  .room(0.4).postgain(pick(gainnn, gooo))
-  ._punchcard({height:200, width:1670})
-
+  .postgain(pick(gainnn, gooo))
+  ._punchcard({height:200, width:1670}).color("lightgreen")
+  
 const arpeggiator = [
   "{d4 bb3 eb3 d3 bb2 eb2}%16",
   "{c4 bb3 f3 c3 bb2 f2}%16",
@@ -38,11 +37,10 @@ main_arp: note(pick(arpeggiator, "<0 1 2 3>".slow(2)))
   .lpf(300)
   .sustain(0.5).release(0.01).attack(0)
   .room(0.6)
-  .lpenv(slider(2.1145, 1.25, 6))
+  .lpenv(slider(4.70325, 1.25, 6))
   .postgain(pick(gainnn, gooo))
-  ._punchcard({height:200, width:1670})
+  ._punchcard({height:200, width:1670}).color("yellow")
 
 drums: stack(
   s("tech:5").postgain(6).pcurve(2).pdec(1).struct(pick(Structures, gooo))
-)._punchcard({height:50, width:1670})
-
+)._punchcard({height:50, width:1670}).color("cyan")
