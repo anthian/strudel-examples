57858fe58e95a2939f6fc0fecd9f05b852296bc2
anthian <raghnarok2002@gmail.com>
2025-08-12 00:12:48 +0200

    Added piano base chords to zelda song of the storms

--- songs/zelda_song_of_the_storms.js
+++ songs/zelda_song_of_the_storms.js
@@ -1,11 +1,46 @@
 // Song of Storms - The Legend of Zelda: Ocarina of Time
-// Translated from guitar tablature to Strudel
+// Translated from music sheet to Strudel
 
-// Main melody pattern
+setCps(180/60/4)
 
-setCps(170/60/4)
+PIANO: cat(
+  note("[d2@2 [f3,a4] [f3,a4]]"),
+  note("[e2@2 [g3,b4]@2]"),
+  note("[f2@2 [a4,c4] [a4,c4]]"),
+  note("[e2@2 [g3,b4]@2]"),
+  note("[d2@2 [f3,a4] [f3,a4]]"),
+  note("[e2@2 [g3,b4]@2]"),
+  note("[f2@2 [a4,c4] [a4,c4]]"),
+  note("[e2@2 [g3,b4]@2]"),
+  note("[d2@2 [f3,a4]@2]"),
+  note("[e2@2 [g3,b4]@2]"),
+  note("[f2@2 [a4,c4]@2]"),
+  note("[e2@2 [g3,b4]@2]"),
+  note("[a#2@2 [d3,f3]@2]"),
+  note("[f3@2 [a4,c4]@2]"),
+  note("[a#2@2 [d3,f3]@2]"),
+  note("[a2@2 [c#3,e3]@2]"),
+  note("[d2@2 [f3,a4]@2]"),
+  note("[e2@2 [g3,b4]@2]"),
+  note("[f2@2 [a4,c4]@2]"),
+  note("[e2@2 [g3,b4]@2]"),
+  note("[a#2@2 [d3,f3]@2]"),
+  note("[f3@2 [a4,c4]@2]"),
+  note("[a#2@2 [d3,f3]@2]"),
+  note("[a2@2 [c#3,e3]@2]")
+).sound("piano")
+.lpf(800)
+.room(0.2)
 
-cat(
+ACCORDION: cat(
+    note(~!4),
+    note(~!4),
+    note(~!4),
+    note(~!4),
+    note(~!4),
+    note(~!4),
+    note(~!4),
+    note(~!4),
     note("[d4 f4] d5@2"),
     note("[d4 f4] d5@2"),
     note("e5@1.5 f5@0.5 [e5 f5]"),
@@ -20,7 +55,9 @@ cat(
     note("[e5 c5] a4@2"),
     note("a4 d4 [f4 g4]"),
     note("a4@2 a4"),
-    note("d4@6 ~")
+    note("d4@6 ~"),
+    note("~")
 )
 .sound("gm_accordion")
+.lpf(4000)
 .room(0.2)
